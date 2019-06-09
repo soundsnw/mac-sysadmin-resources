b070428acc51b95ff35b6c6b442b8ea8966eb702
snowsnd <35668108+snowsnd@users.noreply.github.com>
2019-06-09 19:35:13 +0200

    Sophos installation Script
    Sophos installation Script

---
+++ scripts/sophos-installation.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#
+# Sophos Installation Script
+#
+# Assumes pre-deployment of Sophos Installer.app to 
+# /usr/local/deploy/sophos/
+#
+
+# Fix permissions on installer
+/usr/sbin/chown -R root:wheel "/usr/local/deploy/sophos/Sophos Installer.app"
+/bin/chmod a-w "/usr/local/deploy/sophos/Sophos Installer.app"
+/bin/chmod a+x "/usr/local/deploy/sophos/Sophos Installer.app/Contents/MacOS/Sophos Installer"
+/bin/chmod a+x "/usr/local/deploy/sophos/Sophos Installer.app/Contents/MacOS/tools/com.sophos.bootstrap.helper"
+
+# Run the Sophos installer
+sudo /usr/local/deploy/sophos/Sophos\ Installer.app/Contents/MacOS/Sophos\ Installer --install
+
+# Cleanup
+/bin/rm -rf "/usr/local/deploy/sophos"
+/usr/bin/killall "Sophos Installer"
+
+exit 0
