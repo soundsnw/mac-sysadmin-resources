011f8339a175ad2b1466ad1d6e6186b76bb4120e
snowsnd <35668108+snowsnd@users.noreply.github.com>
2019-06-09 17:43:05 +0200

    Checks if OneDrive has been set up
    Checks if OneDrive has been set up

--- extension-attributes/onedrive-configured.s
+++ extension-attributes/onedrive-configured.s
@@ -4,7 +4,7 @@
 # Jamf Extension Attribute
 #
 # Checks if OneDrive is configured by seeing if the OneDrive folder is present, which would be the case after initial configuration.
-#
+# 
 
 loggedInUser=`python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");'`
 
