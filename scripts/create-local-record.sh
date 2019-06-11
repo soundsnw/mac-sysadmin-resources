944013784c696613755a343014baea56c3469df3
soundsnw <35668108+soundsnw@users.noreply.github.com>
2019-06-11 22:02:52 +0200

    Update create-local-record.sh

--- scripts/create-local-record.sh
+++ scripts/create-local-record.sh
@@ -56,33 +56,3 @@ exit 0
 # defaults write com.microsoft.office OfficeActivationEmailAddress $userEmail
 #
 # (More information: https://macadmins.software)
-
-#
-# Example use: Write information to Jamf computer record
-#
-# To write the asset tag defined below to a Jamf computer record from a local script using the API, use the curl command below
-#
-# Prerequisites:
-# - A Jamf API user specifically for the purpose, as described above, with a strong password and non-standard username.
-#   (Important: Limit write access to what you will update)
-#
-# API users with write access are potentially more destructive than read-only users, and you should, ideally,
-# enable them only while needed and disable them afterwards, and run scripts that write to Jamf records from
-# your admin workstation and not locally, like in this example.
-#
-# Keep in mind that writing to a Jamf computer record will not update a local record like the one created above. 
-# The record would have to be re-downloaded to be up-to-date.
-#
-
-## Define Asset tag
-## Hard-coded here, but you could make a script to match these up with the correct JSS id and write
-## them to the JSS using information from an asset management database and shell commands and utilities 
-## like sed, awk and cut
-# assetTag=562199
-#
-## Get JSS id from local record
-# jssid=$(cat /usr/local/records/computer_info.xml | sed -n -e 's/.*<id>\(.*\)<\/id>.*/\1/p')
-#
-## Write asset tag to computer record
-# /usr/bin/curl --tlsv1.2 -X PUT -H 'authorization: Basic cGeoLe6nuPKwnzwqTAG2WdTLxHzt6Nnd7goV4csz' -H "Content-Type: text/xml" -d "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?> <computer> <general> <asset_tag>${assetTag}</asset_tag> </general> </computer>" https://organization.jamfcloud.com/JSSResource/computers/id/${jssid} > /dev/null
-#
