117250a6f059a76bf1073e54f652f0b5e753b021
soundsnw <35668108+soundsnw@users.noreply.github.com>
2019-08-31 09:40:51 +0200

    Update onedrive-configured.sh

--- extension-attributes/onedrive-configured.sh
+++ extension-attributes/onedrive-configured.sh
@@ -6,7 +6,7 @@
 # Checks if OneDrive is configured by seeing if the OneDrive folder is present, which would be the case after initial configuration.
 # 
 
-loggedInUser=`python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");'`
+loggedInUser=$( scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/ && ! /loginwindow/ { print $3 }' )
 
 if [[ -d "/Users/$loggedInUser/OneDrive" ]] ; then
 	echo "<result>Yes</result>"
