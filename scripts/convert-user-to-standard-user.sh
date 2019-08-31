2ff02efd95fcb3e06ec5c62fc8f5139e2958e4c9
soundsnw <35668108+soundsnw@users.noreply.github.com>
2019-08-31 08:58:13 +0200

    Update convert-user-to-standard-user.sh

--- scripts/convert-user-to-standard-user.sh
+++ scripts/convert-user-to-standard-user.sh
@@ -2,7 +2,7 @@
 
 # Convert logged in user to standard user if the user has admin privileges 
 
-loggedInUser=`python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");'`
+loggedInUser=$( scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/ && ! /loginwindow/ { print $3 }' )
 
 if id -G $loggedInUser | grep -q -w 80 ; 
     then 
