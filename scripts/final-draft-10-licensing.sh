c72164de8c1b6b650ee25e5c2b548019b0440e1b
soundsnw <35668108+soundsnw@users.noreply.github.com>
2019-08-31 09:41:30 +0200

    Update final-draft-10-licensing.sh

--- scripts/final-draft-10-licensing.sh
+++ scripts/final-draft-10-licensing.sh
@@ -10,7 +10,7 @@
 # https://github.com/jamf/Encrypted-Script-Parameters
 #
 
-loggedInUser=$(/usr/bin/python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')
+loggedInUser=$( scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/ && ! /loginwindow/ { print $3 }' )
 
 echo "Deleting any previous licenses";
 
