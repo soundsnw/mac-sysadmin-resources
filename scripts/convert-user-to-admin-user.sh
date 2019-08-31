c20105cf5d784691326d8ad5e5b338c914f8ca0e
soundsnw <35668108+soundsnw@users.noreply.github.com>
2019-08-31 08:57:49 +0200

    Update convert-user-to-admin-user.sh

--- scripts/convert-user-to-admin-user.sh
+++ scripts/convert-user-to-admin-user.sh
@@ -2,6 +2,6 @@
 
 # Convert logged in user to admin user
 
-loggedInUser=`python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");'`
+loggedInUser=$( scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/ && ! /loginwindow/ { print $3 }' )
 
 /usr/sbin/dseditgroup -o edit -a $loggedInUser -t user admin
