7147a1eb9329c61d4644ee4b7334833a1bb4bcc2
soundsnw <35668108+soundsnw@users.noreply.github.com>
2019-09-03 22:13:59 +0200

    Create onedrive-syncfailures.sh

---
+++ extension-attributes/onedrive-syncfailures.sh
@@ -0,0 +1,25 @@
+
+#!/bin/bash
+
+loggedInUser=$( scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/ && ! /loginwindow/ { print $3 }' );
+
+onedriveFolder="/Users/$loggedInUser/OneDrive"
+
+if [[ -d "$onedriveFolder" ]] ; then
+
+rm /tmp/odfailures
+
+find "${onedriveFolder}" -name '*[\\:*?"<>|]*' -print >> /tmp/odfailures
+find "${onedriveFolder}" -name "* " -print >> /tmp/odfailures
+find "${onedriveFolder}" -name "*." -print >> /tmp/odfailures
+find "${onedriveFolder}" -name " *" -print >> /tmp/odfailures
+
+odsyncfailures=$(cat /tmp/odfailures | wc -l | sed -e 's/^ *//')
+
+echo "<result>$odsyncfailures</result>"
+
+else
+
+echo "<result>0</result>"
+
+fi
